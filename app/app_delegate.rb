52e8a03211b82316510218ac40b8f5047312e4c7
Raymond T. Hightower <rth@wisdomgroup.com>
2013-02-03 16:39:21 -0600

    initial commit

---
+++ app/app_delegate.rb
@@ -0,0 +1,9 @@
+class AppDelegate
+  def application(application, didFinishLaunchingWithOptions:launchOptions)
+
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @button = UIButton.buttonWithType(UIButtonTypeRoundedRect)
+
+    true
+  end
+end
