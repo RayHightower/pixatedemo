8fd65a5b6be8ef5f42b071daf91c0c84d82331f5
Raymond T. Hightower <rth@wisdomgroup.com>
2013-02-03 16:41:36 -0600

    added README.mdown

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -2,7 +2,12 @@ class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
 
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+
     @button = UIButton.buttonWithType(UIButtonTypeRoundedRect)
+    @button.setTitle('Hello World', forState:UIControlStateNormal)
+    @button.frame = [[120,50],[90,40]]
+
+    @window.addSubview(@button)
 
     true
   end
